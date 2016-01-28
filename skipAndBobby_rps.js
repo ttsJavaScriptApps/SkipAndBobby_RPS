7896e876494f5ce909a131712e6352bde7cde12f
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-01-28 16:33:19 -0500

    Defined two objects for two players. Each player has name and getHand() properties.

--- skipAndBobby_rps.js
+++ skipAndBobby_rps.js
@@ -3,3 +3,11 @@ function getHand(hands) {
   return hands[parseInt(Math.random()*10)%3]
 }
 console.log(getHand(hands));
+var player1 = {
+  name: 'Bobby',
+  getHand: getHand
+}
+var player2 = {
+  name: 'Skip',
+  getHand: getHand
+}
