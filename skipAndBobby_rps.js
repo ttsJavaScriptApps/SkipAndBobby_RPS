17ab108065f479df471471ed32f47eed95cf85bd
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-01-28 16:55:37 -0500

    playRound function successfully plays hands for player 1 & 2

--- skipAndBobby_rps.js
+++ skipAndBobby_rps.js
@@ -2,7 +2,7 @@ var hands = ['rock','paper', 'scissors']
 function getHand(hands) {
   return hands[parseInt(Math.random()*10)%3]
 }
-console.log(getHand(hands));
+// console.log(getHand(hands));
 var player1 = {
   name: 'Bobby',
   getHand: getHand
@@ -12,5 +12,6 @@ var player2 = {
   getHand: getHand
 }
 function playRound(player1, player2) {
-  
+  player1.getHand(hands);
+  player2.getHand(hands);
 }
