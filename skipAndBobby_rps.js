b5f5ebf761c6a7d3c032053e3941da36a85378a1
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-01-28 17:30:29 -0500

    broken branch

--- skipAndBobby_rps.js
+++ skipAndBobby_rps.js
@@ -14,4 +14,26 @@ var player2 = {
 function playRound(player1, player2) {
   player1.getHand(hands);
   player2.getHand(hands);
+  for (var i = 0; i < 4; i++) {
+    console.log(i);
+    if(player1.getHand(hands) == "rock" && player2.getHand(hands) == "scissors") {
+      console.log(player1.name + " Wins!");
+    } else {
+      console.log(player2.name + " Wins!");
+    }
+    if(player1.getHand(hands) == "paper" && player2.getHand(hands) == "rock") {
+      console.log(player1.name + " Wins!");
+    } else {
+      console.log(player2.name + " Wins!");
+    }
+    if(player1.getHand(hands) == "scissors" && player2.getHand(hands) == "paper") {
+      console.log(player1.name + " Wins!");
+    } else {
+      console.log(player2.name + " Wins!");
+    }
+    if(player1.getHand(hands) == player2.getHand(hands)) {
+      console.log("It's a Tie!");
+    }
+  }
 }
+console.log(playRound(player1, player2));
